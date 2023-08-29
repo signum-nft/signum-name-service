8386dcb59e7c0a2faa06a763a5cc85c5f994ca26
ohager <oliver@devbutze.com>
2023-08-29 19:30:10 -0300

    chore: removed unused env vars

--- app/config.ts
+++ app/config.ts
@@ -30,25 +30,11 @@ export const Config = {
     DefaultNode: ReliableNodes[0],
     DefaultTld: "signum",
   },
-  Contracts: {
-    StakingContract: {
-      Reference: process.env.NEXT_PUBLIC_STAKING_CONTRACT_REFERENCE || "",
-      DeploymentCosts: toNumber(
-        process.env.NEXT_PUBLIC_STAKING_CONTRACT_DEPLOYMENT_COSTS || "2.5"
-      ),
-      CodeHash: process.env.NEXT_PUBLIC_STAKING_CONTRACT_CODE_HASH || "",
-    },
-    LiquidityContract: {
-      PortalFees:
-        process.env.NEXT_PUBLIC_LIQUIDITY_CONTRACT_PORTAL_FEES || "0.05",
-    },
-  },
   Platform: {
     IsMaintenance: toBoolean(process.env.NEXT_PUBLIC_IS_MAINTENANCE || "false"),
     Name: process.env.NEXT_PUBLIC_PLATFORM_NAME || "signumswap.com",
-    MaxAliasLoad: toNumber(process.env.NEXT_PUBLIC_MAX_ALIASES || "10000"),
+    MaxAliasLoad: toNumber(process.env.NEXT_PUBLIC_MAX_ALIASES || "5000"),
     MaxSubdomains: toNumber(process.env.NEXT_PUBLIC_MAX_SUBDOMAINS || "100"),
-    SignumArtUrl: process.env.NEXT_PUBLIC_SIGNUM_ART || "",
     DocumentationUrl:
       process.env.NEXT_PUBLIC_PLATFORM_DOCUMENTATION_URL ||
       "https://docs.signum.network/signumswap",
