b01ac41d5f1ff092395f9ed5ed9759268efc651e
ohager <oliver@devbutze.com>
2023-08-30 09:08:23 -0300

    fix: i18n fixed on vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,9 @@
 import { Home } from "@/features/home";
-// import { GetServerSidePropsI18N, withTranslations } from "@/app/i18n/server";
+import { GetServerSidePropsI18N, withTranslations } from "@/app/i18n/server";
 
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-
-export async function getServerSideProps({ locale }: any) {
-  return {
-    props: {
-      ...(await serverSideTranslations(locale, ["common"])),
-      // Will be passed to the page component as props
-    },
-  };
+export async function getServerSideProps({ locale }: GetServerSidePropsI18N) {
+  return withTranslations(locale)();
 }
-// export async function getServerSideProps({ locale }: GetServerSidePropsI18N) {
-//   return withTranslations(locale)();
-// }
 
 export default function HomePage() {
   return <Home />;
