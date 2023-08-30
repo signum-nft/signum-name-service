76c8ef8aaa1758a2015c06296421dc4f14c1c094
ohager <oliver@devbutze.com>
2023-08-30 08:48:03 -0300

    trying to fix i18n

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
 import { Home } from "@/features/home";
-import { GetServerSidePropsI18N, withTranslations } from "@/app/i18n/server";
+// import { GetServerSidePropsI18N, withTranslations } from "@/app/i18n/server";
 
-export async function getServerSideProps({ locale }: GetServerSidePropsI18N) {
-  return withTranslations(locale)();
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+      // Will be passed to the page component as props
+    },
+  };
 }
+// export async function getServerSideProps({ locale }: GetServerSidePropsI18N) {
+//   return withTranslations(locale)();
+// }
 
 export default function HomePage() {
   return <Home />;
