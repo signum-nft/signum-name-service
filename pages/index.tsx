e0af6c6539ea1a7de5130c14e50c3f0ff62ce88d
ohager <oliver@devbutze.com>
2023-08-30 08:51:37 -0300

    trying to fix i18n - analyzing

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { Home } from "@/features/home";
 
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-export async function getStaticProps({ locale }: any) {
+export async function getServerSideProps({ locale }: any) {
   return {
     props: {
       ...(await serverSideTranslations(locale, ["common"])),
